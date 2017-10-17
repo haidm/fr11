cf0d961e5d2c901fce77ad7387741c33ac16bd82
Admin <dothanhdatkhmt@gmail.com>
2017-10-17 20:43:54 +0700

    Assignment4

---
+++ app/code/local/Datdt/Assignment4/Block/Blockc.php
@@ -0,0 +1,11 @@
+<?php
+
+class Datdt_Assignment4_Block_Blockc extends Mage_Core_Block_Template
+{
+   public function _construct()
+    {
+        $this->setTemplate('assignment4/blockc.phtml');
+    }
+}
+
+?>
--- app/code/local/Datdt/Assignment4/controllers/IndexController.php
+++ app/code/local/Datdt/Assignment4/controllers/IndexController.php
@@ -8,13 +8,19 @@ class Datdt_Assignment4_IndexController extends Mage_Core_Controller_Front_Actio
     public function indexAction()
     {
         
-         $layoutObj = $this->getLayout();
-        $block = $layoutObj->createBlock('myblock/assignment4')->setTemplate('Datdt/assignment4.phtml')->setChild(blocka,$blockaBlock)->setChild(blockb,$blockbBlock);
-
+          $layoutObj = $this->getLayout();
 
           $blockaBlock =$layoutObj->createBlock('myblock/blocka','blocka')->setTemplate('Datdt/blocka.phtml');
 
-           $blockbBlock =$layoutObj->createBlock('myblock/blockb','blockb')->setTemplate('Datdt/blockb.phtml');
+          $blockbBlock =$layoutObj->createBlock('myblock/blockb','blockb')->setTemplate('Datdt/blockb.phtml');
+
+          $blockcBlock =$layoutObj->createBlock('myblock/blockc','blockc')->setTemplate('Datdt/blockc.phtml');
+
+          $blockdBlock =$layoutObj->createBlock('myblock/blockd','blockd')->setTemplate('Datdt/blockd.phtml');
+
+          $blockeBlock =$layoutObj->createBlock('myblock/blocke','blocke')->setTemplate('Datdt/blocke.phtml');
+
+          $block = $layoutObj->createBlock('myblock/assignment4')->setTemplate('Datdt/assignment4.phtml')->setChild(blocka,$blockaBlock)->setChild(blockb,$blockbBlock)->setChild(blockc,$blockcBlock)->setChild(blockd,$blockdBlock)->setChild(blocke,$blockeBlock);
 
         echo $block->toHtml();
     }
