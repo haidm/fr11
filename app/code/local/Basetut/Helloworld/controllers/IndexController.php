3cce12e61568a102460528ec022e100a0d4eba04
trinhuuhien <hienth@smartosc.com>
2017-10-17 19:27:31 +0700

    update ass4

--- app/code/local/Basetut/Helloworld/controllers/IndexController.php
+++ app/code/local/Basetut/Helloworld/controllers/IndexController.php
@@ -23,7 +23,7 @@ class Basetut_Helloworld_IndexController extends Mage_Core_Controller_Front_Acti
         $childBlock4 = $layoutObject->createBlock('wrapBlock/Childblock4')->setTemplate('helloworld/mainright.phtml');
         $childBlock5 = $layoutObject->createBlock('wrapBlock/Childblock5')->setTemplate('helloworld/footer.phtml');
         $block = $layoutObject->createBlock('wrapBlock/helloworld')->setTemplate('helloworld/helloworld.phtml')
-        ->setChild('block_header',$childBlock1)
+        ->setChild('block_header',$childBlock)
         ->setChild('block_mainl',$childBlock2)
         ->setChild('block_mainc',$childBlock3)
         ->setChild('block_mainr',$childBlock4)
