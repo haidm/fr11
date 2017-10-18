402ab87b784b2861eb46bd0214eb6341344b5d46
anhnt81 <anhnt30194@gmail.com>
2017-10-18 16:29:23 +0700

    Assignment_5_anhnt9

--- app/code/local/Anhnt9/Demoproduct/controllers/IndexController.php
+++ app/code/local/Anhnt9/Demoproduct/controllers/IndexController.php
@@ -8,34 +8,7 @@
 class Anhnt9_Demoproduct_IndexController extends Mage_Core_Controller_Front_Action
 {
     public function indexAction(){
-        $layoutObject = $this->getLayout();
-
-        $blockChildHeader = $layoutObject->createBlock('myblock/logo','logo_header');
-        $blockHeader = $layoutObject->createBlock('myblock/header','header')->append($blockChildHeader,'block_header');
-
-        $blockChildLeft = $layoutObject->createBlock('myblock/c','c');
-        $blockLeft = $layoutObject->createBlock('myblock/left','left')->append($blockChildLeft,'block_left');
-
-        $blockChildRight_d = $layoutObject->createBlock('myblock/d','d');
-        $blockRight = $layoutObject->createBlock('myblock/right')
-        ->append($blockChildRight_d,'right_d');
-
-        $blockChildContent_a = $layoutObject->createBlock('myblock/a','a');
-        $blockChildContent_b = $layoutObject->createBlock('myblock/b','b');
-        $blockContent = $layoutObject->createBlock('myblock/content')
-        ->append($blockChildContent_a,'content_a')
-        ->append($blockChildContent_b,'content_b');
-
-        $blockFooter = $layoutObject->createBlock('myblock/footer','footer');
-        $block = $layoutObject->createBlock('myblock/blockTemp')
-            ->setChild('header',$blockHeader)
-            ->setChild('left',$blockLeft)
-            ->setChild('content',$blockContent)
-            ->setChild('right',$blockRight)
-            ->setChild('footer',$blockFooter);
-
-
-        echo $block->toHtml();
-
+        $this->loadLayout('demoproduct_index_index');
+        $this->renderLayout();
     }
 }
