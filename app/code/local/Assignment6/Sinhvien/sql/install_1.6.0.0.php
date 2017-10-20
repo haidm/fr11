4028a42f3752efff4c808c045ac90a00b8192098
nguyenthobao <thobaocss@gmail.com>
2017-10-20 15:16:04 +0700

    baont

--- app/code/local/Assignment6/Sinhvien/sql/install_1.6.0.0.php
+++ app/code/local/Assignment6/Sinhvien/sql/install_1.6.0.0.php
@@ -8,17 +8,17 @@ $installer->startSetup();
  * Create table 'core/resource'
  */
 $table = $installer->getConnection()
-    ->newTable($installer->getTable('Assignment6_Sinhvien/sinhvien'))
+    ->newTable($installer->getTable('assignment6_sinhvien/sinhvien'))
     ->addColumn('id', Varien_Db_Ddl_Table::TYPE_INTEGER, 10, array(
         'nullable'  => false,
         'primary'   => true,
         'identity' =>true,
-         'unsigned' =>true ), 'Id')
+         'unsigned' =>true ))
     ->addColumn('name', Varien_Db_Ddl_Table::TYPE_TEXT, 50, array(
         'nullable'  => false,
         'primary'   => false,
-    ), 'Name')
+    ))
     ->addColumn('email', Varien_Db_Ddl_Table::TYPE_TEXT, 100, array(
-    ), 'Email');
+    ));
 $installer->getConnection()->createTable($table);
 $installer->endSetup();
