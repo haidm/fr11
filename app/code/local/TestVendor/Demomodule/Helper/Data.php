2d27d90bf1cba1570a1bce44c45cd66fbdd25f02
nguyenthobao <thobaocss@gmail.com>
2017-10-24 15:12:01 +0700

    baont

---
+++ app/code/local/TestVendor/Demomodule/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 10/17/2017
+ * Time: 10:43 AM
+ */ 
+class TestVendor_Demomodule_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
